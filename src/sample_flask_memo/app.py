efd7201b14f9bcc3f6da21148f599a3ee6ecf01d
Tadashi Takahashi <ttakahashi@umn.co.jp>
2024-07-01 17:56:26 +0900

    postでデータが受け取れるように修正

--- src/sample_flask_memo/app.py
+++ src/sample_flask_memo/app.py
@@ -1,19 +1,18 @@
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def index():
-    if(request.method == 'POST'):
-        exampleFormControlTextarea1= request.form.get('exampleFormControlTextarea1')
-        return render_template('index.html', exampleFormControlTextarea1=exampleFormControlTextarea1)
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route("/add")
+
+@app.route("/add", methods=["POST"])
 def add():
-    return render_template('index.html')
-    
-print("add")
+    memo = request.form.get("memo")
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True)
