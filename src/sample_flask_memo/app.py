00a61657f09626f451d69f11f4046fc8684b54a3
Haruki Ise <hise@umn.co.jp>
2024-07-17 12:25:51 +0900

    入力したメモが４列になるように修正

--- src/sample_flask_memo/app.py
+++ src/sample_flask_memo/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request
-import psycopg
-import csv
-from sqlalchemy import create_engine
 import os
+from sqlalchemy import create_engine, desc
 from sqlalchemy.orm import sessionmaker
 from sample_flask_memo.infra.models.memo import Memo
+from uuid6 import uuid7
+
 
 app = Flask(__name__)
 
@@ -15,33 +15,19 @@ session = Session()
 
 @app.route("/", methods=["GET"])
 def index():
-
-    memo_list=session.query(Memo).all()
+    memo_list=session.query(Memo).order_by('memo').all()
+    print(memo_list)
     return render_template("index.html", memo_list=memo_list)
-    # with psycopg.connect("host=localhost dbname=memo user=postgres password=postgres") as conn:
-    #     with conn.cursor() as cur:
-    #         cur.execute("SELECT * FROM memo")
-    #         memo_list = cur.fetchall()
-    # return render_template("index.html", memo_list=memo_list)
 
 
 @app.route("/add", methods=["POST"])
 def add():
     memo = request.form.get("memo")
 
-    with open("test_data.csv","r",encoding="utf-8") as csvfile:
-        reader=csv.reader(csvfile)
-        next(reader) #csvファイルの1行目(列名)を除く
-        for row in reader:
-            memo=Memo(id=row[0],memo=row[1])
-            session.add(memo)
-        session.commit()
-
-    # with psycopg.connect("host=localhost dbname=memo user=postgres password=postgres") as conn:
-    #     with conn.cursor() as cur:
-    #         cur.execute("INSERT INTO memo (id, memo) VALUES(%(id)s, %(memo)s)",
-    #                 {"id": uuid7(), "memo": memo})
-    # return index()
+    memo=Memo(id=uuid7(), memo=memo)
+    session.add(memo)
+    session.commit()
+    return index()
 
 
 if __name__ == "__main__":
